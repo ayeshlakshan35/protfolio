45ec1dcf27d8606d7093246d5833e730a72a7fc3
ayeshlakshan35 <ayeshalakshan00@gmail.com>
2026-01-06 16:13:25 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'
-import './App.css'
-import Navbar from './Components/navBar'
-import Footer from './Components/footer'
-import Home from './pages/Home'
-import Resume from './pages/Resume'
-import Services from './pages/Services'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import "./App.css";
+import Navbar from "./Components/navBar";
+import Footer from "./Components/footer";
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+import Services from "./pages/Services";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Services" element={<Services />} />
-          <Route path="/Resume" element={<Resume />} />
-          <Route path="/Projects" element={<Projects />} />
-          <Route path="/Contact" element={<Contact />} />
+    <Router basename="/protfolio">
+      <Navbar />
 
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Resume" element={<Resume />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
 
-        <Footer />
-      </Router>
-    </>
-  )
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
