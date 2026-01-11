f317725fdbf374fd17424ad2334aed2edd92c808
ayeshlakshan35 <ayeshalakshan00@gmail.com>
2026-01-11 23:37:34 +0530

    fix videos and 404 error

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/protfolio/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/protfolio/" : "/",
   server: {
     port: 3000,
     open: true,
   },
   plugins: [react(), tailwindcss()],
-});
+}));
