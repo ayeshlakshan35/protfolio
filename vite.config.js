2ff896f5e165a5344cb9430b18b44694b5bb89c1
ayeshlakshan35 <ayeshalakshan00@gmail.com>
2026-01-12 00:34:01 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/protfolio/" : "/",
-  server: {
-    port: 3000,
-    open: true,
-  },
   plugins: [react(), tailwindcss()],
 }));
